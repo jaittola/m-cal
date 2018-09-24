5aef9d6845948879be6cb8cb42083578658dc37f
Jukka Aittola <jukka.aittola@reaktor.fi>
2018-09-24 19:40:28 +0300

    Clean up backend code;
     * bookings.clj: extract functions

--- src/clj/m_cal/bookings.clj
+++ src/clj/m_cal/bookings.clj
@@ -22,6 +22,34 @@
   (try (Integer/parseInt number-string)
        (catch Exception e nil)))
 
+(defn database-insert-user [connection name yacht_name email]
+  (first (db-insert-user connection
+                         {:name name
+                          :yacht_name yacht_name
+                          :email email})))
+
+(defn database-insert-bookings [connection selected_dates user-id]
+  (doall (mapcat (fn [day]
+                   (db-insert-booking connection
+                                      {:booked_date day
+                                       :users_id (:id user-id)}))
+                 selected_dates)))
+
+(defn database-insert-booking-log [connection dates-to-booking-ids user-id]
+  (doall (map (fn [id-date]
+                (db-insert-booking-log connection
+                                       {:booked_date (:date id-date)
+                                        :users_id (:id user-id)
+                                        :booking_id (:booking_id id-date)
+                                        :booking_or_release log-entry-booking}))
+              dates-to-booking-ids)))
+
+(defn map-dates-to-booking-ids [booking-id-containers selected_dates]
+  (map (fn [booking-id-container date]
+         {:booking_id (:id booking-id-container)
+          :date date})
+       booking-id-containers selected_dates))
+
 (defn load-all-bookings []
   (jdbc/with-db-connection [connection dbspec]
     (db-list-all-bookings connection)))
@@ -42,33 +70,19 @@
         (not (vector? selected_dates))) (error-reply 400 "Mandatory parameters missing.")
     (not (== required-days (count selected_dates))) (error-reply 400 (str "You must book " required-days " days."))
     :else (try
-            (jdbc/with-db-transaction [t-con dbspec]
-              (let [user-id (first (db-insert-user t-con
-                                                   {:name name
-                                                    :yacht_name yacht_name
-                                                    :email email}))
-                    booking_id_containers (doall (mapcat (fn [day]
-                                                           (db-insert-booking t-con
-                                                                              {:booked_date day
-                                                                               :users_id (:id user-id)}))
-                                                         selected_dates))
-                    booking_ids_to_dates (map (fn [booking_id_container date]
-                                                {:booking_id (:id booking_id_container) :date date})
-                                              booking_id_containers selected_dates)]
-                (doall (map (fn [id-date]
-                              (db-insert-booking-log t-con {:booked_date (:date id-date)
-                                                            :users_id (:id user-id)
-                                                            :booking_id (:booking_id id-date)
-                                                            :booking_or_release log-entry-booking}))
-                            booking_ids_to_dates))
-              {:status 200
-               :body {:user {:id (:id user-id)
-                             :key (:secret_id user-id)
-                             :name name
-                             :yacht_name yacht_name
-                             :email email}
-                      :selected_dates selected_dates
-                      :all_bookings (db-list-all-bookings t-con)}}))
+            (jdbc/with-db-transaction [connection dbspec]
+              (let [user-id (database-insert-user connection name yacht_name email)
+                    bookings-ids (database-insert-bookings connection selected_dates user-id)
+                    dates-to-booking-ids (map-dates-to-booking-ids bookings-ids selected_dates)
+                    _ (database-insert-booking-log connection dates-to-booking-ids user-id)]
+                {:status 200
+                 :body {:user {:id (:id user-id)
+                               :key (:secret_id user-id)
+                               :name name
+                               :yacht_name yacht_name
+                               :email email}
+                        :selected_dates selected_dates
+                        :all_bookings (db-list-all-bookings connection)}}))
             (catch PSQLException pse
               (let [se (.getServerErrorMessage pse)
                     sqlstate (.getSQLState se)]
