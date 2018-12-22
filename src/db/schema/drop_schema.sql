d0333d6b2e1e27da853e4551b1f813ae16dbf760
Jukka Aittola <jaittola@iki.fi>
2018-12-22 11:26:14 +0200

    Fix database cleanup script

--- src/db/schema/drop_schema.sql
+++ src/db/schema/drop_schema.sql
@@ -1,4 +1,4 @@
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS email_confirmation_queue;
 DROP TABLE IF EXISTS booking_log;
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS users;
