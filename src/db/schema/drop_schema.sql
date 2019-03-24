7c4e923cf36d978e3c0ca687f4129919e36687b3
Jukka Aittola <jaittola@iki.fi>
2019-03-24 09:26:37 +0200

    Add the dropping of database functions to drop_schema.sql

--- src/db/schema/drop_schema.sql
+++ src/db/schema/drop_schema.sql
@@ -4,3 +4,6 @@ DROP TABLE IF EXISTS email_confirmation_queue;
 DROP TABLE IF EXISTS booking_log;
 DROP TABLE IF EXISTS booking;
 DROP TABLE IF EXISTS users;
+
+DROP FUNCTION IF EXISTS check_password;
+DROP FUNCTION IF EXISTS create_user;
