4881b5e9cd1ffdc43afedd97f8e08f7611aa37e1
Jukka Aittola <jukka.aittola@reaktor.com>
2020-02-22 13:43:39 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,34 +1,35 @@
 (defproject m-cal "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.339"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
                  [org.clojure/spec.alpha "0.2.176"]
-                 [reagent "0.8.1"]
-                 [reagent-utils "0.3.2"]
+                 [reagent "0.9.1"]
+                 [reagent-utils "0.3.3"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [cljs-http "0.1.45"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [ring/ring-devel "1.6.3"]
-                 [ring/ring-json "0.4.0"]
+                 [cljs-http "0.1.46"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [ring/ring-devel "1.8.0"]
+                 [ring/ring-json "0.5.0"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-ssl "0.3.0"]
                  [compojure "1.6.1"]
                  [environ "1.1.0"]
-                 [com.layerware/hugsql "0.4.9"]
-                 [org.postgresql/postgresql "42.2.5"]
-                 [org.clojure/java.jdbc "0.7.8"]
+                 [com.layerware/hugsql "0.5.1"]
+                 [org.postgresql/postgresql "42.2.10"]
+                 [org.clojure/java.jdbc "0.7.11"]
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]
                  [com.cemerick/url "0.1.1"]
                  [cljsjs/babel-polyfill "6.20.0-2"]
-                 [clj-http "3.9.1"]
-                 [clj-time "0.15.0"]
-                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "3.10.0"]
+                 [clj-time "0.15.2"]
+                 [org.clojure/data.json "1.0.0"]
                  [alxlit/autoclave "0.2.0" :exclusions [com.google.guava/guava]]
                  [crypto-random "1.2.0"]
-                 [org.apache.poi/poi-ooxml "3.17"]]
+                 [org.apache.poi/poi-ooxml "4.1.2"]]
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-less "1.7.5"]
-            [lein-ring "0.12.4"]]
+            [lein-ring "0.12.4"]
+            [lein-ancient "0.6.15"]]
 
   :min-lein-version "2.5.3"
   :uberjar-name "m-cal-standalone.jar"
@@ -47,12 +48,12 @@
          :target-path  "resources/public/css"}
 
   :profiles {:dev
-             {:dependencies [[figwheel-sidecar "0.5.18"]
-                             [binaryage/devtools "0.9.10"]
-                             [cider/piggieback "0.3.10"]
-                             [ring/ring-mock "0.3.2"]]
-              :plugins      [[lein-figwheel "0.5.18"]
-                             [cider/cider-nrepl "0.18.0"]]}
+             {:dependencies [[figwheel-sidecar "0.5.19"]
+                             [binaryage/devtools "1.0.0"]
+                             [cider/piggieback "0.4.2"]
+                             [ring/ring-mock "0.4.0"]]
+              :plugins      [[lein-figwheel "0.5.19"]
+                             [cider/cider-nrepl "0.24.0"]]}
 
              :test
              {:test-paths ["test/clj"]}
